f39a21b928225579566602e415dc5c93efb9a1d6
Luis Cervantes <lcervant@hotmail.com>
2017-12-13 05:17:58 -0500

    new information

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,82 +1,51 @@
 $(document).ready(function() {
-
-	var stoneOneValue, stoneTwoValue, stibeThreeValue, stoneFourValue;
 	var wins = 0;
 	var losses = 0;
 
-	var randomComputerNumber;
-	
-	var userTallyScore;
+	var $crystals = $('#crystals img')
 
-	function initializeVariables() {
-		
-		randomComputerNumber = 19 + Math.floor(Math.random() * 102);
-		
-		stoneOneValue = 1 + Math.floor(Math.random() * 12);
-		stoneTwoValue = 1 + Math.floor(Math.random() * 12);
-		stoneThreeValue = 1 + Math.floor(Math.random() * 12);
-		stoneFourValue = 1 + Math.floor(Math.random() * 12);
-		
-		userTallyScore = 0;
-		
-		$("#winsTally").html("Wins: " + wins);
-		$("#lossesTally").html("Losses: " + losses);
-		$("#randomNumber").html(randomComputerNumber);
-		$("#userScore").html(userTallyScore);
-		consoleLogVariables();
+	// Generate number value for images
+	generateCrystalNumbers($crystals)
+
+	var generateNewGame = function() {
+		var counter = 0;
+		var randomNumber = 19 + Math.floor(Math.random() * 102)
+
+		$('#randomNumber .value').text(randomNumber)
+
+		$crystals.on('click', function() {
+			var crystalValue = $(this).data('value')
+			counter = counter + crystalValue
+
+			$('#yourScore').text(counter)
+
+			if (randomNumber === counter) {
+				wins++
+				$('#win').text(wins)
+				$('#yourScore').text('0')
+				generateCrystalNumbers($crystals)
+				generateNewGame()
+			}
+
+			if (counter > randomNumber) {
+				losses++
+				$('#loss').text(losses)
+				$('#yourScore').text('0')
+				generateCrystalNumbers($crystals)
+				generateNewGame()
+			}
+		})
 	}
 
-	function hasUserWonOrLost() {
-	
-		if (userTallyScore > randomComputerNumber) {
-			losses++;
-			console.log("user lost");
-			initializeVariables();
-		}
-
-		
-		if (userTallyScore == randomComputerNumber) {
-			wins++;
-			console.log("user won");
-			initializeVariables();
-		}
+	// Generates random number for each image.
+	function generateCrystalNumbers(els) {
+		els.each(function(index, el) {
+			var randomNumber = 1 + Math.ceil(Math.random()*12)
+			$(el).attr('data-value', randomNumber)
+		})
 	}
 
+	// Initialize game
+	generateNewGame()
 
-	function consoleLogVariables() {
-		console.log("wins: " + wins + " losses: " + losses);
-		console.log("stoneOneValue: ", stoneOneValue + " stoneTwoValue: " + stoneTwoValue + " stoneThreeValue: " + stoneThreeValue + " stoneFourValue: " + stoneFourValue);
-		console.log("randomComputerNumber: " + randomComputerNumber + " userTallyScore: " + userTallyScore);
-		console.log("----------------------------------");
-
-	}
-
-	
-
-	initializeVariables();
-
-	
-	$(".stone").on("click", function() {
-		
-		var pressed = $(this).attr("value");
-        console.log(pressed);
-      
-        if (pressed == "stone1") {
-        	userTallyScore += stoneOneValue;
-        } else if (pressed == "stone2") {
-        	userTallyScore += stoneTwoValue;
-        } else if (pressed == "stone3") {
-        	userTallyScore += stoneThreeValue;
-        } else if (pressed == "stone4") {
-        	userTallyScore += stoneFourValue;
-        } else {
-        	console.log("error");
-        }
-        
-        $("#userScore").html(userTallyScore);
-        consoleLogVariables();
-       
-        hasUserWonOrLost();
-	});
-
-});
+})
